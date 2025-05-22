291dc9f27904149152b310017b5929d184b88a89
RichardR5 <145227444+RichardR5@users.noreply.github.com>
2025-05-22 13:36:03 +0300

    Update apiparingud.js

--- src/apiparingud.js
+++ src/apiparingud.js
@@ -99,12 +99,12 @@ async function handleFormSubmit(event) {
 }
 
 async function listiraamatud() {
-    const responseData = await getDataAsJson("http://localhost:5000/raamatud/");
+    const responseData = await getDataAsJson("https://praks12-raamatud-a6fjemh0f8d4a8gv.northeurope-01.azurewebsites.net/raamatud/");
     const resultElement = document.getElementById("raamatud_result");
     resultElement.innerHTML = ""
     for (var raamat of responseData.raamatud){
-        resultElement.innerHTML += '<a href="http://localhost:5000/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
-                '<a href="#" onclick="deleteObject(\'http://localhost:5000/raamatud/'+raamat+'\')" > [kustuta]</a>' +
+        resultElement.innerHTML += '<a href="https://praks12-raamatud-a6fjemh0f8d4a8gv.northeurope-01.azurewebsites.net/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
+                '<a href="#" onclick="deleteObject(\'https://praks12-raamatud-a6fjemh0f8d4a8gv.northeurope-01.azurewebsites.net/raamatud/'+raamat+'\')" > [kustuta]</a>' +
                 "<br />";
     }
 }
